877f8440f1acc96516ea4c5d24928d059d8bab0d
danieljgorski <danieljgorski@gmail.com>
2022-05-08 20:09:04 +0200

    developed results directories and scripts until final-clustering

--- scripts/0-Hmmr-OE-complete-analysis.R
+++ scripts/0-Hmmr-OE-complete-analysis.R
@@ -1,7 +1,10 @@
 # make directories
 output_dirs <- c(
   "results",
-  "results/objects"
+  "results/objects",
+  "results/preprocessing",
+  "results/doublet-removal",
+  "results/post-clustering-qc"
 )
 
 for (i in output_dirs) {
@@ -14,4 +17,9 @@ for (i in output_dirs) {
 }
 
 # run analysis in order
-source("scripts/1-Hmmr-OE-pre-processing.R")
+source("scripts/1-Hmmr-OE-preprocessing.R")
+source("scripts/2-Hmmr-OE-doublet-classification.R")
+source("scripts/3-Hmmr-OE-doublet-removal.R")
+source("scripts/4-Hmmr-OE-initial-clustering.R")
+source("scripts/5-Hmmr-OE-post-clustering-qc.R")
+#source("scripts/6-Hmmr-OE-final-clustering.R")
