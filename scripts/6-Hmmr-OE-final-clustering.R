1f6d901763e0d33de1576239ee8f441458b5db58
danieljgorski <danieljgorski@gmail.com>
2022-05-14 13:43:46 +0200

    remove unnecessary which() function

--- scripts/6-Hmmr-OE-final-clustering.R
+++ scripts/6-Hmmr-OE-final-clustering.R
@@ -6,17 +6,45 @@ library(Seurat) #v4.0.1
 # Load object
 load("results/objects/obj.Rdata")
 
+# Exclude low-quality clusters determined during post-clustering-qc
+obj <- subset(x = obj, idents = "33", invert = TRUE)
+
+# Remove extra meta data columns
+obj@meta.data <- select(obj@meta.data,
+                        -starts_with("pANN"),
+                        -c(nCount_SCT,
+                           nFeature_SCT,
+                           SCT_snn_res.0.8,
+                           seurat_clusters,
+                           doublet_classification))
+
+# Keep only necessary counts and data slots
+obj <- DietSeurat(obj,
+                  counts = T,
+                  data = T,
+                  scale.data = F,
+                  features = NULL,
+                  assays = "RNA",
+                  dimreducs = NULL,
+                  graphs = NULL)
+
+# Save pruned object
+save(obj, file = "results/objects/obj.Rdata")
+
+# Clear memory
+rm(list = ls())
+gc()
+
+# Load cleaned object
+load("results/objects/obj.Rdata")
+
 # SCTransform
 obj <- SCTransform(obj, verbose = T, variable.features.n = 3002)
 
 # Take Hmmr and FIJ5 out of variable features
 var_features <- VariableFeatures(obj, assay = "SCT")
-which(var_features == "Hmmr")
-which(var_features == "FIJ5")
 var_features <- var_features[var_features != "Hmmr"]
 var_features <- var_features[var_features != "FIJ5"]
-which(var_features == "Hmmr")
-which(var_features == "FIJ5")
 VariableFeatures(obj, assay = "SCT") <- var_features
 
 # PCA
