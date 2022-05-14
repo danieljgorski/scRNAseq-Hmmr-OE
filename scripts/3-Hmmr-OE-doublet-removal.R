e79b2c68fe3651033c3fdae8b82eb0d2f53f2b2f
danieljgorski <danieljgorski@gmail.com>
2022-05-14 13:41:30 +0200

    loading / saving obj with stage specific naming, change to 25 PC etc.

--- scripts/3-Hmmr-OE-doublet-removal.R
+++ scripts/3-Hmmr-OE-doublet-removal.R
@@ -2,15 +2,16 @@
 
 # Load libraries
 library(Seurat) #v4.0.1
+library(dplyr)
 
 # Load object
-load("results/objects/obj.Rdata")
+load("results/objects/obj_db_classified.Rdata")
 
 # Cluster merged object to visualize doublets
 obj <- SCTransform(obj, verbose = T)
 obj <- RunPCA(obj, verbose = T)
-obj <- RunUMAP(obj, reduction = "pca", dims = 1:30, verbose = T)
-obj <- FindNeighbors(obj, dims = 1:30, verbose = T)
+obj <- RunUMAP(obj, reduction = "pca", dims = 1:25, verbose = T)
+obj <- FindNeighbors(obj, dims = 1:25, verbose = T)
 obj <- FindClusters(obj, resolution = 0.8, verbose = T)
 obj@meta.data$doublet_classification <- factor(obj@meta.data$doublet_classification,
                                                levels = c("Singlet", "Doublet"))
@@ -20,6 +21,32 @@ pdf(file = "results/doublet-removal/DimPlot_doublet_classification.pdf",
 print(p)
 dev.off()
 
-# Subset for singlets and save object
+# Subset for singlets
 obj <- subset(x = obj, subset = doublet_classification == "Singlet")
-save(obj, file = "results/objects/obj.Rdata")
+
+# Remove extra meta data columns
+obj@meta.data <- select(obj@meta.data,
+                        -starts_with("pANN"),
+                        -c(nCount_SCT,
+                           nFeature_SCT,
+                           SCT_snn_res.0.8,
+                           seurat_clusters,
+                           doublet_classification))
+
+# Keep only necessary counts and data slots
+DefaultAssay(obj) <- "RNA"
+obj <- DietSeurat(obj,
+                  counts = T,
+                  data = T,
+                  scale.data = F,
+                  features = NULL,
+                  assays = "RNA",
+                  dimreducs = NULL,
+                  graphs = NULL)
+
+# Save slimmed down object with no doublets
+save(obj, file = "results/objects/obj_db_removed.Rdata")
+
+# Clear memory
+rm(list = ls())
+gc()
