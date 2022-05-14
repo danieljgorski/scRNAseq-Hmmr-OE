43ad3d8ea88bbb6f2fd671b49295cd1687eec107
danieljgorski <danieljgorski@gmail.com>
2022-05-14 13:38:58 +0200

    expand to basic annotation

--- scripts/0-Hmmr-OE-complete-analysis.R
+++ scripts/0-Hmmr-OE-complete-analysis.R
@@ -1,13 +1,13 @@
 # Complete analysis of scRNAseq-Hmmr-OE data
 
 # Make directories
-output_dirs <- c(
-  "results",
-  "results/objects",
-  "results/preprocessing",
-  "results/doublet-removal",
-  "results/post-clustering-qc"
-)
+output_dirs <- c("results",
+                 "results/objects",
+                 "results/preprocessing",
+                 "results/doublet-removal",
+                 "results/post-clustering-qc",
+                 "results/basic-annotation"
+                 )
 
 for (i in output_dirs) {
   if (!dir.exists(i)) {
@@ -22,6 +22,7 @@ for (i in output_dirs) {
 source("scripts/1-Hmmr-OE-preprocessing.R")
 source("scripts/2-Hmmr-OE-doublet-classification.R")
 source("scripts/3-Hmmr-OE-doublet-removal.R")
-source("scripts/4-Hmmr-OE-initial-clustering.R")
-source("scripts/5-Hmmr-OE-post-clustering-qc.R")
-source("scripts/6-Hmmr-OE-final-clustering.R")
+source("scripts/4-Hmmr-OE-initial-integrated-clustering.R")
+#source("scripts/5-Hmmr-OE-post-clustering-qc.R")
+#source("scripts/6-Hmmr-OE-final-clustering.R")
+#source("scripts/6-Hmmr-OE-basic-annotation.R")
