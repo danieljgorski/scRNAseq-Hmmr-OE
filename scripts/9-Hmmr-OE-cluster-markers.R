ff217650a5b70b8b988101cec2c01490ef81c0c2
danieljgorski <danieljgorski@gmail.com>
2022-05-27 16:43:07 +0200

    heatmap redone with ComplexHeatmap package

--- scripts/9-Hmmr-OE-cluster-markers.R
+++ scripts/9-Hmmr-OE-cluster-markers.R
@@ -5,7 +5,9 @@ library(Seurat) #>=4.0.1
 library(ggplot2)
 library(dplyr)
 library(ggrepel)
+library(ComplexHeatmap)
 source("scripts/colors.R")
+source("scripts/dimplotlevels.R")
 
 # Load object
 load("results/objects/obj_annotated.Rdata")
@@ -24,36 +26,76 @@ write.csv(cluster_markers,
           row.names = F)
 
 # Heatmap
+
+# Down sample to fit column titles and reduce size
+obj_down <- subset(obj, downsample = 100)
+
+# Filter top 20 marker genes, and top 1 for labeling
 top20 <- cluster_markers %>%
   group_by(cluster) %>%
   top_n(n = 20, wt = avg_log2FC)
-Idents(obj) <- "basic_annotation"
-p <- DoHeatmap(subset(obj, downsample = 500), 
-               features = top20$gene,
-               label = F,
-               assay = "RNA",
-               group.colors = colors,
-               group.by = "basic_annotation") +
-  scale_fill_gradient2(low = "#2664ad", mid = "white", high = "#eb2a0e") +
-  ylab("Marker genes") +
-  theme(axis.text.y = element_blank(),
-        axis.title.y = element_text(size = 18),
-        legend.text= element_text(size= 12),
-        legend.title = element_text(size = 18),
-        legend.justification = "top") +
-  scale_color_manual(values = colors) +
-  guides(color = guide_legend(title = "Identity",
-                              override.aes = list(shape=16,
-                                                  size=4.25,
-                                                  alpha=1)))
-pdf(file = "results/cluster-markers/Heatmap_top_20.pdf",
-    height = 9,
-    width = 7,
+top20 <- top20[!duplicated(top20$gene),]
+top2 <- top20 %>%
+  group_by(cluster) %>%
+  top_n(n = 2, wt = avg_log2FC)
+top2 <- top2[!duplicated(top2$gene),]
+
+# Filter out duplicates
+top20 <- top20[!duplicated(top20$gene),]
+top2 <- top2[!duplicated(top2$gene),]
+
+# Extract markers from scaled data slot
+mat <- obj_down[["RNA"]]@scale.data[top20$gene, ] %>% as.matrix()
+
+# Find scaled data range and create custom colors
+quantile(mat, c(0.05, 0.95))
+cols = circlize::colorRamp2(c(-2, 0, 2), c("#2664ad", "white", "#eb2a0e"))
+
+# Build annotations for column and row splitting
+cluster_anno <- obj_down@meta.data$basic_annotation
+cluster_anno <- factor(cluster_anno, levels = dimplotlevels)
+top <- HeatmapAnnotation(foo = anno_block(gp = gpar(fill = colors),
+                                          height = unit(4, "mm")))
+right <- rowAnnotation(foo = anno_block(gp = gpar(fill = colors),
+                                        width = unit(2, "mm")),
+                       bar = anno_mark(which(rownames(mat) %in%
+                                                   top2$gene),
+                                           labels = top2$gene,
+                                           labels_gp = gpar(fontsize = 10, 
+                                                            fontface = "italic")))
+
+# Plot heatmap
+p <- Heatmap(mat,
+        name = "Scaled expression",
+        col = cols,
+        top_annotation = top,
+        column_split = cluster_anno,
+        column_gap = unit(0.6, "mm"),
+        cluster_columns = F,
+        show_column_names = F,
+        column_title_gp = gpar(fontsize = 14),
+        column_title_rot = 90,
+        right_annotation = right,
+        row_split = factor(top20$cluster, levels = dimplotlevels),
+        row_gap = unit(0.6, "mm"),
+        cluster_rows = F,
+        show_row_names = F,
+        row_title = NULL,
+        use_raster = T,
+        raster_quality = 4,
+        heatmap_width = unit(24, "cm"),
+        heatmap_height = unit(27, "cm"),
+        heatmap_legend_param = list(direction = "horizontal",
+                                    title_gp = gpar(fontize = 2),
+                                    title_position = "lefttop"))
+pdf(file = "results/cluster-markers/Heatmap.pdf",
+    width = 10,
+    height = 12,
     useDingbats = F)
-print(p)
+draw(p, heatmap_legend_side = "bottom")
 dev.off()
 
-# Return averaged expression values for each identity class, convert to df
+# Return averaged expression values for each identity class, convert to a df
 avg_exp <- AverageExpression(obj, assays = "RNA", group.by = "basic_annotation")
 avg_exp <- avg_exp$RNA
 avg_exp <- as.data.frame(avg_exp)
