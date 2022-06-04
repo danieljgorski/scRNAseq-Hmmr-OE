b94bd2873901c180beb58a57012d22e380f58ead
danieljgorski <danieljgorski@gmail.com>
2022-06-04 21:48:09 +0200

    Remove make directories

--- scripts/0-Hmmr-OE-complete-analysis.R
+++ scripts/0-Hmmr-OE-complete-analysis.R
@@ -1,23 +1,5 @@
 # Complete analysis of scRNAseq-Hmmr-OE data
 
-# Make directories
-output_dirs <- c("results",
-                 "results/objects",
-                 "results/preprocessing",
-                 "results/doublet-removal",
-                 "results/post-clustering-qc",
-                 "results/basic-annotation"
-                 )
-
-for (i in output_dirs) {
-  if (!dir.exists(i)) {
-    dir.create(i)
-    print(paste0("made ", i, " directory"))
-  } else {
-    print(paste0(i, " directory already exists."))
-  }
-}
-
 # Run analysis in order
 source("scripts/1-Hmmr-OE-preprocessing.R")
 source("scripts/2-Hmmr-OE-doublet-classification.R")
